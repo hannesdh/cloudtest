e262148150f6fbadbe406dc7c7fb0b7552c360b1
Mr. Hannes <hannes.dhulster@wijs.be>
2016-08-19 11:32:40 +0200

    alles derin

---
+++ index.php
@@ -0,0 +1,280 @@
+<!DOCTYPE HTML>
+<!--
+	Solid State by Pixelarity
+	pixelarity.com | hello@pixelarity.com
+	License: pixelarity.com/license
+-->
+<html>
+	<head>
+		<title>HackFridays</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+
+		
+
+				<!-- Banner -->
+					<section id="banner">
+						<div class="inner">
+							<h2> &lt;/&gt; HackFridays</h2>
+							<p>Georchestreerde hands-on hackathons op vrijdag voor developers en mensen (zoals marketeers en CEO's) <br/>rond development, architectuur, internet of things, AR, VR, iBeacons en lean thinking.
+								</p>
+						</div>
+					</section>
+				
+				<!-- Wrapper -->
+					<section id="wrapper">
+
+						<!-- One - Learn -->
+							<section id="one" class="wrapper spotlight style2">
+								<div class="inner">
+								<img src="images/leer.jpg" alt="Sebastiaan legt uit" class="image"/>
+									<div class="content">
+										<h2 class="major">Leer</h2>
+										<p>Ze leren nieuwe dingen. Onze experts komen langs en werken rond een bepaald thema. Deelnemers krijgen genoeg nieuwe info om op onderzoek uit te gaan in een heel nieuwe wereld. Zo worden worden nieuwe technologieën, frameworks en methodologieën aangereikt die ook na de sessie blijven leven.
+										</p>
+									
+									</div>
+								</div>
+							</section>
+
+						<!-- Two - Play -->
+							<section id="two" class="wrapper alt spotlight style3">
+								<div class="inner">
+									<img src="images/speel2.jpg" alt="Arduino en ledjes" class="image"/>
+									<div class="content">
+										<h2 class="major">Speel</h2>
+										<p>We laten ze vooral zelf aan de slag gaan.Wanneer de basisconcepten gekend zijn is het tijd om het zelf te doen. We bouwen een eigen app, aangestuurd door door een sensor. Marketing en IT werken samen een Bitcoin concept uit of we maken een eerste VR experience. Het resultaat is soms minder belangrijk, het onderzoeken des te meer.</p>
+									
+									</div>
+								</div>
+							</section>
+
+						<!-- Three - produce -->
+							<section id="three" class="wrapper spotlight style4">
+								<div class="inner">
+								<img src="images/creeer.jpg" alt="Lasercust box for Raspberri Pi 3" class="image"/>
+									<div class="content">
+										<h2 class="major">Creëer</h2>
+										<p>We produceren een concreet eindresultaat. Tijd om  een probleem uit de organisatie op te lossen. We bepalen een MVP of POC en prototypen iets waarmee we publiek en management mee kunnen charmeren.</p>
+									
+									</div>
+								</div>
+							</section>
+
+					
+							
+							
+						
+							<!-- Five - Aanbod -->
+								<section id="five" class="wrapper alt style7">
+									<div class="inner">
+										
+											<div class="box alt">
+												<div class="row uniform">
+												
+													<div class="4u 12u$(small)">
+														<div class="argument"><span class="icon fa-rocket"></span></div>
+														<h3>Innoveer</h3>
+														<p>Stimuleer innovatief denken en handelen door samen aan een concrete oplossing te bouwen. </p>
+													</div>
+													<div class="4u 12u$(small)">
+														<div class="argument"><span class="icon fa-star"></span></div>
+														<h3>Verover talent</h3>
+														<p>Maak het interessant voor technische profielen bij jullie te werken en te blijven.</p>
+													</div>
+													<div class="4u$ 12u$(small)"><div class="argument"><span class="icon fa-university"></span></div>
+													<h3>Update de Kennis</h3>
+													<p>Leer de nieuwste technieken van experts die er dagelijk mee bezig zijn</p>
+												</div>
+								
+												</div>
+												<div class="spacer"></div>
+												<h2 class="major">Kies maar uit</h2>
+													<div class="row uniform">
+														<div class="12u$"><span class="image fit"><img src="images/stukjes.png" alt="" /></span></div>
+
+														<div class="3u 12u$(small)">	
+															<h3>Development</h3>
+															<ul class="alt">
+																<li>3-layer architecture</li>
+																<li>API’s</li>
+																<li>Front-end Frameworks</li>
+																<li>Back-end Frameworks</li>
+																<li>Mobile Frameworks</li>
+																<li>Git flow</li>
+																<li>Docker</li>
+																<li>ElasticSearch</li>
+																<li>PHP/Node/Python/Swift/…</li>
+																<li>Laravel/React/…</li>
+															</ul>
+															
+														</div>
+															<div class="3u 12u$(small)">
+																<h3>UX/UI</h3>
+																<ul class="alt">
+																	<li>UX for Front-enders</li>
+																</ul>
+																<h3>IoT</h3>	
+																<ul class="alt">
+																	<li>Arduino 101, Advanced</li>
+																	<li>Raspberry Pi</li>
+																	<li>iBeacons</li>
+																
+																</ul>
+																	<h3>Blockchain</h3>	
+																	<ul class="alt">
+																		<li>Blockchain 101</li>
+																		<li>Bitcoin 101</li>
+
+																	</ul>
+																
+															</div>
+																<div class="3u 12u$(small)">	
+																		<h3>SaaS</h3>	
+																	<ul class="alt">
+																		<li>Middleware</li>
+																		<li>Scalability</li>
+																		<li>Google API’s</li>
+																		<li>Microsoft API’s</li>
+																	</ul>
+																		<h3>Games</h3>		
+																	<ul class="alt">
+																		<li>AR Gamekit</li>
+																		<li>VR Gamekit</li>
+																		<li>Unity 101</li>
+																	</ul>
+																</div>
+																	<div class="3u$ 12u$(small)">
+																			<h3>Lean</h3>		
+																		<ul class="alt">
+																			<li>Lean Startup</li>
+																			<li>MVP</li>
+																			<li>Biz Model Canvas</li>
+																		</ul>
+																	</div>
+
+													</div>
+
+									</div>
+						
+								</section>
+								
+						
+									</section>
+
+								<!-- Seven - Teamleader -->
+									<section id="seven" class="wrapper spotlight style5">
+										<div class="inner">
+											<img src="images/simon.jpg" alt="Simon Floré" class="image"/>
+											<div class="content">
+												<h2 class="major">Simon Floré, CTO bij Teamleader</h2>
+												<p>De transitie naar de user interface V2 in de vorm van een succesvol hackathon weekend, waarna Cloudoki deelprojecten is gaan bouwen, en er een tweede hackathon-twee-daagse kwam om het nieuwe front-end framework te kunnen valideren (in groep).</p>
+											
+											</div>
+										</div>
+									</section>
+							
+									<!-- Six - Tui -->
+										<section id="six" class="wrapper alt spotlight style6">
+											<div class="inner">
+											<img src="images/kurt.jpg" alt="Kurt Rogiers" class="image"/>
+												<div class="content">
+													<h2 class="major">Kurt Rogiers, Innovation Architect bij Tui/JetAir</h2>
+													<p>Kurt kwam met de vraag naar hackathon-trajecten om innovatie te stimuleren binnen de organisatie, en om de developers te inspireren. Dit is gegroeid naar twee sessies, om de drie weken, in zowel Tui BE als Tui NL, met concrete interesse uit Tui Nordics en andere takken.</p>
+
+												</div>
+											</div>
+							<!-- end sections -->
+
+					</section>
+
+				<!-- Footer -->
+					<section id="footer">
+						<div class="inner">
+							<a name="formsent"></a>
+							<?php
+							if($_POST['sent']){
+								
+								$to = 'hannes@nerdlab.be';
+
+								$subject = 'Bericht vanuit de hackfridays website';
+
+								$headers = "From: " . strip_tags($_POST['email']) . "\r\n";
+								$headers .= "Reply-To: ". strip_tags($_POST['email']) . "\r\n";
+
+								$headers .= "MIME-Version: 1.0\r\n";
+								$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+
+								$message .= 'Hannes,<br></br>';
+
+								$message.='naam: '.$_POST['name'].'<br> emailadres: '.$_POST['email'].'<br> telnr: '.$_POST['phone'].'<br> bericht: '.$_POST['message'].'<br> <br>Dat heb je dan weer puik gedaan.' ;
+								mail($to, $subject, $message, $headers);
+								
+								
+								echo "<p class='button special fit good'>Dank voor bericht</p>";
+							}
+							
+							?>
+							<h2 class="major">Klaar voor HackFridays?</h2>
+							<p>Contacteer ons met je (aan)vraag en wij verzorgen een antwoord op maat.</p>
+							<form method="post" action="#formsent">
+								<div class="field">
+									<label for="name">Je Naam</label>
+									<input type="text" name="name" id="name" />
+								</div>
+								<div class="field">
+									<label for="email">Je mailadres</label>
+									<input type="email" name="email" id="email" />
+								</div>
+									<div class="field">
+										<label for="phone">Je telefoonnummer</label>
+										<input type="tel" name="phone" id="phone" />
+									</div>
+								<div class="field">
+									<label for="message">Je vraag</label>
+									<textarea name="message" id="message" rows="4"></textarea>
+								</div>
+								<ul class="actions">
+									<li><input type="submit" name="sent" value="Verstuur bericht" class="special"/></li>
+								</ul>
+							</form>
+							<ul class="contact">
+							
+							<li class="fa-phone">(Bram zijn nummer) 000-0000</li>
+								<li class="fa-envelope"><a href="#">hannes@hacKfridays.coM</a></li>
+								<li class="fa-twitter"><a href="#">twitter.com/hackfridays</a></li>
+								<li class="fa-facebook"><a href="#">facebook.com/hackfridays</a></li>
+								<li class="fa-instagram"><a href="#">instagram.com/hackfridays</a></li>
+									<li class="fa-home">
+										Cloudoki BVBA<br />
+										Molenaarsstraat 111, 45B<br />
+										9000 Gent
+									</li>
+							</ul>
+							<ul class="copyright">
+								<li>&copy; Cloudoki. All rights reserved.</li>
+							</ul>
+						</div>
+					</section>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
